9644b2e6b6cd06656d6cb587c510abd82c534f20
cybermax-008 <surya.murugaian@gmail.com>
2020-03-01 14:04:52 -0500

    Start Commit

--- app/src/main/java/com/example/digiwall/LoginActivity.java
+++ app/src/main/java/com/example/digiwall/LoginActivity.java
@@ -31,145 +31,14 @@ import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String TAG = "AndroidClarified";
-    private SignInButton googleSignInButton;
-    private GoogleSignInClient googleSignInClient;
-
-    //intitializing the variables in registration page
-    private EditText mEmail;
-    private EditText mPass;
-    private Button btnLogin;
-    private TextView mForgotPassword;
-    private TextView mSignupHere;
-
-    private ProgressDialog mDialog;
-
-    // Firebase
-    private FirebaseAuth mAuth;
-    private FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //assigninig Login variables given by the user.
-        mEmail = findViewById(R.id.email_login);
-        mPass = findViewById(R.id.password_login);
-        btnLogin = findViewById(R.id.btn_login);
-        mForgotPassword = findViewById(R.id.forgot_password);
-        mSignupHere = findViewById(R.id.btn_noaccount);
-        //Assigning google signIn btn
-        googleSignInButton = findViewById(R.id.sign_in_button);
-
-        // Start Firebase Instance
-        mAuth=FirebaseAuth.getInstance();
-        mUser=mAuth.getCurrentUser();
-
-        if(mUser != null && mUser.isEmailVerified()){
-
-            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            finish();
-        }
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        mDialog = new ProgressDialog(this);
-
-
-        //Login Button
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LoginActivity.this.loginDetails();
-            }
-        });
-
-        //Google btn
-        googleSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent signInIntent = googleSignInClient.getSignInIntent();
-                startActivityForResult(signInIntent, 101);
-            }
-        });
-
-        //Don't have an account? Register
-        mSignupHere.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), SignupActivity.class));
-            }
-        });
-
-        // Forgot Password?
-        mForgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), ResetActivity.class));
-            }
-        });
-
-    }
-
-    //Login
-    private void loginDetails() {
-
-        String email = mEmail.getText().toString().trim();
-        final String pass = mPass.getText().toString().trim();
-
-        if (TextUtils.isEmpty(email)) {
-            mEmail.setError("Valid Email Required!");
-
-            return;
-        }
-
-        if (TextUtils.isEmpty(pass)) {
-            mPass.setError("Valid Password Required!");
-
-            return;
-        }
-
-        mDialog.setMessage("Processing..");
-        mDialog.show();
-
-        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    if(mAuth.getCurrentUser().isEmailVerified()){
-                        mDialog.dismiss();
-                        showToast("Login Successful!");
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        LoginActivity.this.finish();
-                    }
-                    else{
-                        mDialog.dismiss();
-                        showToast("Email not verified yet!");
-                    }
-
-                } else {
-                    mDialog.dismiss();
-                    if (pass.length() < 6) {
-                        showToast(getString(R.string.minimum_password));
-                    } else {
-                        showToast(getString(R.string.auth_failed));
-                    }
-
-                }
-            }
-        });
-
 
     }
 
 
-
-    public void showToast(String toastText) {
-        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
-    }
 }
