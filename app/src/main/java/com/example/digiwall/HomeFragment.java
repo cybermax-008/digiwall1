392c273ba327e37d8ea7e694380beec3fcf8a104
SANKEERTH26 <54898037+SANKEERTH26@users.noreply.github.com>
2020-04-06 05:14:59 -0400

    Version 3

--- app/src/main/java/com/example/digiwall/HomeFragment.java
+++ app/src/main/java/com/example/digiwall/HomeFragment.java
@@ -80,3 +80,43 @@ public class HomeFragment extends Fragment {
     private FirebaseAuth mAuth;
     private DatabaseReference mIncomeDatabase;
     private DatabaseReference mExpenseDatabase;
+
+@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        myview= inflater.inflate(R.layout.fragment_home, container, false);
+
+        mAuth=FirebaseAuth.getInstance();
+        FirebaseUser mUser=mAuth.getCurrentUser();
+        String uid=mUser.getUid();
+
+        //Intialize Income and expense button
+        final Button incomeButton=myview.findViewById(R.id.btn_income);
+        final Button expenseButton=myview.findViewById(R.id.btn_expense);
+
+        mIncomeDatabase= FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+        mExpenseDatabase= FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+
+        mIncomeDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int totalvalue=0;
+
+                for(DataSnapshot mysnapshot: dataSnapshot.getChildren() ){
+
+                    Data data =mysnapshot.getValue(Data.class);
+
+                    totalvalue+=data.getAmount();
+
+                    String stTotalvalue= String.valueOf(totalvalue);
+                    incomeButton.setText("Income\n"+stTotalvalue+" CAD");
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
