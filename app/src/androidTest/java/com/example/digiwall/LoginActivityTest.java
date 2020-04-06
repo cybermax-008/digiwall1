3b599ff88c0a9d2810bcd5064ca72ee5e76b0192
Sai Sankeerth Reddy Madem <saisankeerthreddymadem@Sais-MacBook-Pro.local>
2020-04-05 20:55:36 -0400

    Login activity first test

--- app/src/androidTest/java/com/example/digiwall/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/digiwall/LoginActivityTest.java
@@ -1,17 +1,45 @@
 package com.example.digiwall;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertNotNull;
 
 public class LoginActivityTest {
+    @Rule
+    public ActivityTestRule<LoginActivity> mlogin=new ActivityTestRule<LoginActivity>(LoginActivity.class);
+    private LoginActivity mActivity = null;
+    Instrumentation.ActivityMonitor MainActivitymonitor = getInstrumentation().addMonitor(MainActivity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor SigupActivitymonitor = getInstrumentation().addMonitor(SignupActivity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor ResetActivitymonitor = getInstrumentation().addMonitor(ResetActivity.class.getName(),null,false);
 
     @Before
     public void setUp() throws Exception {
+        mActivity = mlogin.getActivity();
     }
-
+    @Test
+    public void buttonCheck() {
+        View view = mActivity.findViewById(R.id.btn_login);
+        assertNotNull(view);
+    }
     @After
     public void tearDown() throws Exception {
+        mActivity =null;
     }
+
 }
